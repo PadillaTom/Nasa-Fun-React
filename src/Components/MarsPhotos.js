70f3d5da01d370454d71784b65469454906ac1bc
Tomas Padilla <padillatomasagustin@gmail.com>
2020-10-26 15:47:58 +0100

    done

--- src/Components/MarsPhotos.js
+++ src/Components/MarsPhotos.js
@@ -5,26 +5,79 @@ const apiKey = process.env.REACT_APP_API_KEY;
 
 const MarsPhotos = ({ nameForApi, solDay }) => {
   // Set Photos
-  const [frontPhotos, setFrontPhotos] = useState();
-  console.log(solDay);
-  console.log(frontPhotos);
+  const [frontPhotos, setFrontPhotos] = useState(null);
+  const [rearPhotos, setRearPhotos] = useState(null);
 
+  // Front Camera = FHAZ
   useEffect(() => {
-    fetchPhotos();
-    async function fetchPhotos() {
+    fetchFrontPhotos();
+    async function fetchFrontPhotos() {
       const response = await fetch(
         `https://api.nasa.gov/mars-photos/api/v1/rovers/${nameForApi}/photos?sol=${solDay}&camera=fhaz&api_key=${apiKey}`
       );
       const data = await response.json();
       setFrontPhotos(data.photos[0]);
     }
-  }, []);
+  }, [nameForApi, solDay]);
 
-  if (frontPhotos === undefined) {
+  // Rear Camera =
+  useEffect(() => {
+    fetchRearPhotos();
+    async function fetchRearPhotos() {
+      const response = await fetch(
+        `https://api.nasa.gov/mars-photos/api/v1/rovers/${nameForApi}/photos?sol=${solDay}&camera=rhaz&api_key=${apiKey}`
+      );
+      const data = await response.json();
+      setRearPhotos(data.photos[0]);
+    }
+  }, [nameForApi, solDay]);
+
+  if (frontPhotos === null || rearPhotos === null) {
     return <Loading></Loading>;
   }
+  return (
+    <React.Fragment>
+      <h2 className='current-display'>Displaying {nameForApi}'s Photos</h2>
 
-  return <section className='section marsphoto-sect'></section>;
+      <article className='photos-container'>
+        {/* Front Camera */}
+        <div className='front-camera'>
+          {frontPhotos?.img_src ? (
+            <React.Fragment>
+              <img
+                src={frontPhotos?.img_src}
+                alt='No Photo of Such SOL'
+                className='camera-photo'
+              />
+              <p className='camera-name'>Front Hazard Avoidance Camera</p>
+              <p className='earth-date'>{frontPhotos?.earth_date}</p>
+            </React.Fragment>
+          ) : (
+            <h4 className='no-sol-photos'>
+              There are no photos for such SOL, please try another one!{' '}
+            </h4>
+          )}
+        </div>
+        {/* Rear Camera */}
+        <div className='rear-camera'>
+          {rearPhotos?.img_src ? (
+            <React.Fragment>
+              {' '}
+              <img
+                src={rearPhotos?.img_src}
+                alt='No Photo of Such SOL'
+                className='camera-photo'
+              />
+              <p className='camera-name'> Rear Hazard Avoidance Camera</p>
+              <p className='earth-date'>{rearPhotos?.earth_date}</p>
+            </React.Fragment>
+          ) : (
+            <h2></h2>
+          )}
+        </div>
+      </article>
+    </React.Fragment>
+  );
 };
 
 export default MarsPhotos;
